d349deed68cdcbe9c843124ba23bbdf0b687ce90
ajeet singh thakur <ajeetsingh930@gmail.com>
2022-07-09 03:04:02 +0530

    new build

--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -77,8 +77,8 @@ export default function DashboardScreen() {
               <Card>
                 <Card.Body>
                   <Card.Title>
-                    {summary.orders && summary.users[0]
-                      ? summary.orders[0].numOrders
+                    {summary.orders && summary?.users[0]
+                      ? summary?.orders[0]?.numOrders
                       : 0}
                   </Card.Title>
                   <Card.Text> Orders</Card.Text>
@@ -91,7 +91,7 @@ export default function DashboardScreen() {
                   <Card.Title>
                     $
                     {summary.orders && summary.users[0]
-                      ? summary.orders[0].totalSales.toFixed(2)
+                      ? summary?.orders[0]?.totalSales?.toFixed(2)
                       : 0}
                   </Card.Title>
                   <Card.Text> Orders</Card.Text>
